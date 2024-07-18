183fa905c42d645bdc73a74f1b000d04012b6a41
Dmitriy Stroganov <distrog@gmail.com>
2024-07-18 19:49:20 +0700

    Initial commit

---
+++ src/test/java/ru/stroganov/test/titanic/TitanicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stroganov.test.titanic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
